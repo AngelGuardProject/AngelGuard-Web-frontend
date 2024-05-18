c7c963e33a5f8968de23e92ef1d2520e55812913
xevin03 <xevin03@naver.com>
2024-05-18 14:09:53 +0900

    Revert "Reapply "내정보수정, 아기정보 수정까지 퍼블 완료""
    This reverts commit fc376e1db9464ea914886a32d01cda6e738cc79b.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import Babyinfo from "./pages/Babyinfo";
-import MyPage from "./pages/MyPage";
-import Main from "./pages/main";
 
 function App() {
     return (
         <div className="App">
-            <Babyinfo />
+            <h1>Hello world!!!</h1>
         </div>
     );
 }
