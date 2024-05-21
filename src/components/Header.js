09ba2aaa81a07e53689775d9b7439366e916e01a
hyerim03 <chhari0708@naver.com>
2024-05-21 15:49:13 +0900

    file 복구

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,32 @@
 import style from "../styles/Header.module.css"; // Correct import path
 function Header() {
-    //메인 헤더
-    return (
-        <div>
-            {/* <div className={style.background}>
-        <div className={style.loginWrap}>
-          <div className={style.left}>
-            <div>AngelGuard</div>
-          </div>
-          <div className={style.right}>
-            <div>
-              <div>Login</div>
-              <input className={style.id} />
-              <input className={style.pw} />
-            </div>
-          </div>
-        </div>
-      </div> */}
-            <header className={style.headerContainer}>
-                <div className={style.logo}>AngelGuard</div>
-                <nav className={style.menu}>
-                    <ul>
-                        <li>
-                            <a href="#" className={style.menuItem}>
-                                community
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className={style.menuItem}>
-                                signup
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className={style.menuItem2}>
-                                login
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
-    );
+  //메인 헤더
+  return (
+    <div>
+      <header className={style.headerContainer}>
+        <div className={style.logo}>AngelGuard</div>
+        <nav className={style.menu}>
+          <ul>
+            <li>
+              <a href="#" className={style.menuItem}>
+                community
+              </a>
+            </li>
+            <li>
+              <a href="#" className={style.menuItem}>
+                signup
+              </a>
+            </li>
+            <li>
+              <a href="#" className={style.menuItem2}>
+                login
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
