c2a6dff707275c3f4f8dc717145a437ffca306be
xevin03 <xevin03@naver.com>
2024-05-24 17:31:44 +0900

    Header중앙 설정

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import { useState } from "react";
 import style from "../styles/Header.module.css"; // Correct import path
 import Login from "./modal/Login-modal";
 import SignUp from "./modal/SignUp-modal";
-function Header() {
+
+function Header({ scrolled, backgroundColor }) {
+    const headerStyle = {
+        backgroundColor: scrolled ? "rgba(255, 244, 214, 0.5)" : backgroundColor,
+    };
+
     const [modal, setModal] = useState();
 
     const openLogin = () => {
@@ -12,36 +17,34 @@ function Header() {
     const openSignUp = () => {
         setModal("signup");
     };
-
-
-  const closeModal = () => {
-    setModal(null);
-  };
-  //메인 헤더
-  return (
-    <div>
-      {modal == "login" ? <Login closeModal={closeModal} /> : null}
-      {modal == "signup" ? <SignUp closeModal={closeModal} /> : null}
-      <header className={style.headerContainer}>
-        <div className={style.logo}>AngelGuard</div>
-        <nav className={style.menu}>
-          <ul>
-            <li>
-              <a href="/community" className={style.menuItem}>
-                community
-              </a>
-            </li>
-            <li onClick={openSignUp} className={style.menuItem}>
-              signup
-            </li>
-            <li onClick={openLogin} className={style.menuItem2}>
-              login
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
-  );
+    const closeModal = () => {
+        setModal(null);
+    };
+    //메인 헤더
+    return (
+        <div>
+            {modal == "login" ? <Login closeModal={closeModal} /> : null}
+            {modal == "signup" ? <SignUp closeModal={closeModal} /> : null}
+            <header className={style.headerContainer} style={headerStyle}>
+                <div className={style.logo}>AngelGuard</div>
+                <nav className={style.menu}>
+                    <ul>
+                        <li>
+                            <a href="/community" className={style.menuItem}>
+                                community
+                            </a>
+                        </li>
+                        <li onClick={openSignUp} className={style.menuItem}>
+                            signup
+                        </li>
+                        <li onClick={openLogin} className={style.menuItem2}>
+                            login
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
 }
 
 export default Header;
